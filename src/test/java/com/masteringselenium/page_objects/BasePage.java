92538af8c6943c400efba3edf6658d3220e5cf80
Marek Kozlovsky <marek.kozlovsky@etnetera.cz>
2019-02-10 11:24:51 +0100

    Common test base.

--- src/test/java/com/masteringselenium/page_objects/BasePage.java
+++ src/test/java/com/masteringselenium/page_objects/BasePage.java
@@ -5,11 +5,25 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class BasePage {
+    protected final WebDriver driver;
 
-    protected RemoteWebDriver driver;
-
-    public BasePage() throws MalformedURLException {
-        //driver = DriverBase.getDriver();
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
     }
+
+    public final void initFields() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public abstract void waitFor();
 }
---
+++ src/test/java/com/masteringselenium/page_objects/pelikan/PelikanHomePage.java
@@ -0,0 +1,85 @@
+package com.masteringselenium.page_objects.pelikan;
+
+import com.masteringselenium.page_objects.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PelikanHomePage extends BasePage {
+
+    public static final String PELIKAN_HOMEPAGE_URL = "https://www.pelikan.cz/cs";
+
+    @FindBy(how = How.ID, using = "//*[@id=\"tab_tickets\" and contains(@class, \"active\")]")
+    private WebElement ticketSearchElement;
+
+    @FindBy(how = How.XPATH, using = "//ul[@class = \"list-tickets\"]/li[1]")
+    private WebElement firstSalesTicketElement;
+
+    @FindBy(how = How.XPATH, using = "//div[contains(@class, \"lang-holder\")]//div[contains(@class, \"list-item-content\")]//span[contains(@class, \"current-lang\")]")
+    private WebElement languageElement;
+
+    @FindBy(how = How.XPATH, using = "//div[contains(@class, \"lang-holder\")]//div[contains(@class, \"list-item-content\")]//span[contains(@class, \"currency\")]")
+    private WebElement currencyElement;
+
+
+    public static PelikanHomePage open(WebDriver driver) {
+        driver.get(PELIKAN_HOMEPAGE_URL);
+
+        PelikanHomePage homePage = new PelikanHomePage(driver);
+        homePage.waitFor();
+        homePage.initFields();
+        return homePage;
+    }
+
+    private PelikanHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void waitFor() {
+
+        // wait 10 seconds at most for the homepage to load all the flight tickets
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+        wait.until((ExpectedCondition<Boolean>) d -> {
+            String saleTickets = d.findElement(By.xpath("//div[contains(@class, 'inner') and contains(@class ,'tickets')]/h3/span")).getText();
+            return !saleTickets.isEmpty();
+        });
+    }
+
+    /**
+     * Gets the first sales ticket in the list.
+     */
+    public WebElement getFirstSalesTicketWebElement() {
+        return firstSalesTicketElement;
+    }
+
+    /**
+     * Gets the ticket search div element.
+     */
+    public WebElement getTicketSearchElement() {
+        return ticketSearchElement;
+    }
+
+    /**
+     * Get currently used language abbreviation.
+     *
+     * @return "CS" etc.
+     */
+    public String getCurrentLang() {
+        return languageElement.getText();
+    }
+
+    /**
+     * Get currently used currency abbreviation.
+     *
+     * @return "CZK" etc.
+     */
+    public String getCurrency() {
+        return currencyElement.getText();
+    }
+}
