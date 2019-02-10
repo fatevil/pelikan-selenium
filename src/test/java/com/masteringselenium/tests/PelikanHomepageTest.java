cc3a894feebc79a5a1af9afddecc6c701fe04e84
Marek Kozlovsky <marek.kozlovsky@etnetera.cz>
2019-02-10 11:25:13 +0100

    Pelikan homepage test.

---
+++ src/test/java/com/masteringselenium/tests/PelikanHomepageTest.java
@@ -0,0 +1,53 @@
+package com.masteringselenium.tests;
+
+import com.masteringselenium.DriverBase;
+import com.masteringselenium.page_objects.pelikan.PelikanHomePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PelikanHomepageTest extends DriverBase {
+
+    private PelikanHomePage homePage;
+
+    /**
+     * Open homepage and assure that the selected language and currency is czech.
+     */
+    @Test
+    public void czechHomepageLanded_successful() {
+        this.homePage = PelikanHomePage.open(driver);
+
+        assertThat(homePage.getCurrency()).isEqualTo("(CZK)");
+        assertThat(homePage.getCurrentLang()).isEqualTo("CS");
+    }
+
+    /**
+     * Assure that there is ticket search div and it's active and visible.
+     */
+    @Test(dependsOnMethods = "czechHomepageLanded_successful")
+    public void ticketSearchIsPresent() {
+        WebElement ticketSearch = homePage.getFirstSalesTicketWebElement();
+        assertThat(ticketSearch.isDisplayed());
+    }
+
+
+    /**
+     * Assure that there is sales tickets div. Put focus on that element if necessary.
+     */
+    @Test(dependsOnMethods = "czechHomepageLanded_successful")
+    public void salesTicketsListIsPresent() {
+        WebElement salesTicketElement = homePage.getFirstSalesTicketWebElement();
+
+        // move to element
+        Actions actions = new Actions(driver);
+        actions.moveToElement(salesTicketElement);
+        actions.perform();
+
+        assertThat(salesTicketElement.isDisplayed());
+    }
+
+}
+
